a8110f0e49285484d0a4e3a1d578574e567e9ca1
Raphael Budd <woder@wltd.org>
2016-11-09 10:10:21 -0500

    Fixed include.js

--- include.js
+++ include.js
@@ -1,26 +1,17 @@
-function w3IncludeHTML() {
-  var z, i, a, file, xhttp;
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    if (z[i].getAttribute("w3-include-html")) {
-      a = z[i].cloneNode(false);
-      file = z[i].getAttribute("w3-include-html");
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-          a.removeAttribute("w3-include-html");
-          a.innerHTML = xhttp.responseText;
-
-          z[i].parentNode.replaceChild(a, z[i]);
-          w3IncludeHTML();
-        }
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
       }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
     }
-  }
-}
+  });
+});
 
 // Make it work with hover over td not img
 
