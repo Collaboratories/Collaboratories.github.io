bc8724a63751d7b7738461b012771fc29fe1c624
Baljinder Benipal <baljinder.benipal@outlook.com>
2016-11-08 19:32:53 -0500

    Update include.js
    Remove hover and unhover functions

--- include.js
+++ include.js
@@ -21,26 +21,3 @@ function w3IncludeHTML() {
     }
   }
 }
-
-// Make it work with hover over td not img
-
-// Baljinder Benipal
-
-// Creates a hover effect with each of the profile pictures for the About page
-function hover(element) {
-  // Stores the id corresponding to which member is hovered over
-  var childElement = element.firstChild;
-  // Changes image source depending on 'name' to display appropriate executive
-  childElement.setAttribute('src', 'wolfpackExecutives/' + childElement + '.jpg');
-  // Sets border-radius to 5em for aesthetic appeal
-  childElement.style.borderRadius = "5em";
-  // Add class to alter background for profile section
-  element.className += " isHover";
-}
-// Changes image back to logo upon mouseout
-function unhover(element) {
-  var childElement = element.firstChild;
-  childElement.setAttribute('src', 'New_WolfPack_Logo.svg');
-  childElement.style.borderRadius = "0em";
-  element.className = element.className.split(" ")[0];
-}
